74ffef11ec6c8e2760b30ad13b837c77090f5d0d
M&C Web Solutions <mandcwebsolutions@gmail.com>
2026-04-12 14:10:30 +0200

    style: adjust mobile menu spacer to 70px for tighter layout

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -125,7 +125,7 @@ const Navbar = () => {
               ))}
             </nav>
             
-            <div className="h-24 w-full" aria-hidden="true" />
+            <div className="h-[70px] w-full" aria-hidden="true" />
 
             {/* Actions Section */}
             <motion.div 
